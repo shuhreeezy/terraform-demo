74202359818fc2fc9f81e724d20303e0af42bbf6
shuhreeezy <120708325+shuhreeezy@users.noreply.github.com>
2023-06-20 17:44:04 -0700

    added db_private_subnets

---
+++ RDS/main.tf
@@ -0,0 +1,18 @@
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+
+  config = {
+    organization = "shuhreeezy"
+    workspaces = {
+      name = "my-vpc"
+    }
+  }
+}
+
+
+
+
+resource "aws_db_subnet_group" "default" {
+  name       = "project-db-subnet-group"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
+}
